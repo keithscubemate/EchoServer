d251fa48ca3f2e8ac71906ef4c8e477f1fe46b47
Austin Jones <austin.jones@uster.com>
2024-03-15 15:06:45 -0400

    works

---
+++ EchoClient/Program.fs
@@ -0,0 +1,33 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+open System.IO
+open System.Net
+open System.Net.Sockets
+let clientLoop (client: TcpClient, read: StreamReader, write: StreamWriter) = 
+    while true do
+        let input = Console.ReadLine()
+        match input with
+        | "" | null ->
+            read.Close()
+            write.Close()
+            client.Close()
+            exit 0
+        | _ ->
+            write.WriteLine(input)
+            let output = read.ReadLine()
+            printfn $"%s{output}"
+        
+[<EntryPoint>]
+let main(args) =
+    if (args.Length <> 2) then
+        exit 1
+    let addr = IPAddress.Parse(args[0])
+    let port = args[1] |> int
+    let client = new TcpClient()
+    client.Connect(addr, port)
+    let dataStream = client.GetStream()
+    let read = new StreamReader(dataStream)
+    let write = new StreamWriter(dataStream, AutoFlush=true)
+    clientLoop(client, read, write)
+    0
---
+++ EchoServer/Program.fs
@@ -0,0 +1,39 @@
+﻿open System
+open System.IO
+open System.Net
+open System.Net.Sockets
+
+let rec echoLoop(client: TcpClient, read: StreamReader, write: StreamWriter) = async {
+    let line = read.ReadLine()
+    match line with
+    | "" | null ->
+        read.Close()
+        write.Close()
+        client.Close()
+    | _ ->
+        printfn $"%s{line}"
+        write.WriteLine(line)
+        return! echoLoop(client, read, write)
+}
+
+let listenerLoop (listener: TcpListener) = async {
+    while true do
+        let client = listener.AcceptTcpClient()
+        printfn "Got Connection"
+        let dataStream = client.GetStream()
+        let read = new StreamReader(dataStream)
+        let write = new StreamWriter(dataStream, AutoFlush=true)
+        Async.Start(echoLoop(client, read, write))
+}
+
+
+[<EntryPoint>]
+let main(args) =
+    if (args.Length <> 1) then
+        exit 1
+    let addr = IPAddress.Any
+    let port = args[0] |> int
+    let listener = new TcpListener(addr, port)
+    listener.Start()
+    Async.RunSynchronously(listenerLoop(listener))
+    0
